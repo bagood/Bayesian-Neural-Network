fd1224e23bfbed5960b29c2f14fa685c989d2f1c
Ariabagus Mustikoatmodjo <ariabagus@Ariabaguss-MacBook-Air.local>
2024-04-28 21:54:18 +0700

    simplify the code and added comments to the code

--- BNN_Forward_Propagation.py
+++ BNN_Forward_Propagation.py
@@ -9,17 +9,44 @@ class bnn_forward_propagation():
         return
 
     def _calculate_ma_i(self, mz_i_1, m_i):
+        """
+        calculate the ith layer input marginal mean
+        Args:
+        mz_i_1 (float) - the (i-1)th layer output marginal mean
+        m_i (float) - the ith layer weight's mean
+        """
         return (m_i @ mz_i_1) / (len(m_i) ** 0.5)
 
     def _calculate_va_i(self, mz_i_1, vz_i_1, m_i, v_i):
+        """
+        calculate the ith layer input marginal variance
+        Args:
+        mz_i_1 (float) - the (i-1)th layer output marginal mean
+        vz_i_1 (float) - the (i-1)th layer output marginal variance
+        m_i (float) - the ith layer weight's mean
+        v_i (float) - the ith layer weight's variance
+        """
         return  (((m_i * m_i) @ vz_i_1) \
                     + (v_i @ (mz_i_1 * mz_i_1)) ) \
                         / (len(mz_i_1) ** 0.5)
 
     def _calculate_alpha(self, ma_i, va_i):
+        """
+        calculate the ith layer alpha value
+        Args:
+        ma_i (float) - the ith layer input marginal mean
+        va_i (float) - the ith layer input marginal variance
+        """
         return ma_i / (va_i ** 0.5)
 
     def _calculate_gaussian_cdf(self, ma_i, va_i, minus):
+        """
+        calculate the ith layer gaussian cumulative distributive function
+        Args:
+        ma_i (float) - the ith layer input marginal mean
+        va_i (float) - the ith layer input marginal variance
+        minus (bool) - ture if the alpha is negative
+        """
         if minus:
             alpha = -1 * self._calculate_alpha(ma_i, va_i)
         else:
@@ -28,50 +55,94 @@ class bnn_forward_propagation():
         return norm.cdf(alpha)
 
     def _calculate_gaussian_pdf(self, ma_i, va_i):
+        """
+        calculate the ith layer gaussian probability density function
+        Args:
+        ma_i (float) - the ith layer input marginal mean
+        va_i (float) - the ith layer input marginal variance
+        """
         alpha = self._calculate_alpha(ma_i, va_i)
         
         return norm.pdf(alpha)
 
     def _calculate_gamma(self, cdf, pdf):
+        """
+        calculate the ith layer gamma value
+        Args:
+        cdf (float) - the ith layer gaussian cumulative distributive function
+        pdf (float) - the ith layer gaussian probability density function
+        """
         return pdf / cdf
         
     def _calculate_mz_i(self, ma_i, va_i, cdf, gamma):
+        """
+        calculate the ith layer output mariginal mean
+        Args:
+        ma_i (float) - the ith layer input marginal mean
+        va_i (float) - the ith layer input marginal variance
+        cdf (float) - the ith layer gaussian cumulative distributive function
+        gamma (float) - the ith layer gamma value
+        """
         return cdf * (ma_i + ((va_i ** 0.5) * gamma))
 
     def _calculate_vz_i(self, ma_i, va_i, mz_i, cdf, minus_cdf, gamma, alpha):
+        """
+        calculate the ith layer output mariginal mean
+        Args:
+        ma_i (float) - the ith layer input marginal mean
+        va_i (float) - the ith layer input marginal variance
+        mz_i (float) - the ith layer output marginal mean
+        cdf (float) - the ith layer gaussian cumulative distributive function
+        minus_cdf (float) - the ith layer gaussian cumulative distributive function
+        gamma (float) - the ith layer gamma value
+        alpha (float) - the ith layer alpha value
+        """
         return (mz_i * (ma_i + ((va_i ** 0.5) * gamma)) * minus_cdf) \
                     + (cdf * va_i * (np.ones(len(ma_i)).reshape(-1, 1) - (gamma ** 0.5) - (gamma * alpha)))
 
     def forward_propagation(self, feature_data_i, m, v, model_structure):
-        ma = []
-        va = []
-        cdf = []
-        minus_cdf = []
-        pdf = []
-        gamma = []
-        alpha = []
-        mz = [feature_data_i / np.max(feature_data_i)]
+        """
+        perform forward propagation to acquire all the necessary variables
+
+        Args:
+        feature_data_i (float) - the current feature_data
+        m (float) - the model weight's mean
+        v (float) - the model weight's variance
+        model_structure (float) - list containing number of neurons on each layers
+        """
+        # empty list to store all variables
+        ma, va, cdf, minus_cdf, pdf, gamma, alpha = [], [], [], [], [], [], []
+        # the output marginal mean for the 0th layer is feature_data
+        mz = [feature_data_i]
+        # the output marginal variance for the 0th layer is zero matrix
         vz = [np.zeros((model_structure[0], 1))]
         
+        # iterate over each layers in the model
         for i in range(len(model_structure)-1):
             ma.append(self._calculate_ma_i(mz[i], m[i]))
             va.append(self._calculate_va_i(mz[i], vz[i], m[i], v[i]))
-            
             cdf.append(self._calculate_gaussian_cdf(ma[i], va[i], False))
             minus_cdf.append(self._calculate_gaussian_cdf(ma[i], va[i], True))
             pdf.append(self._calculate_gaussian_pdf(ma[i], va[i]))
             gamma.append(self._calculate_gamma(cdf[i], pdf[i]))
             alpha.append(self._calculate_alpha(ma[i], va[i]))
-            
             mz.append(self._calculate_mz_i(ma[i], va[i], cdf[i], gamma[i]))
             vz.append(self._calculate_vz_i(ma[i], va[i], mz[i+1], cdf[i], minus_cdf[i], gamma[i], alpha[i]))
                 
         return (ma, va, cdf, minus_cdf, pdf, gamma, alpha, mz, vz)
 
-    def feed_forward_neural_network(self, param, feature_data_i):
-        neuron_values = [feature_data_i]
-        for p in param:
-            layers = p @ neuron_values[-1]
+    def feed_forward_neural_network(self, weight, feature_data_i):
+        """
+        perform feed forward to acquire prediction using the model
+        the feature data is transformed using an exponential function and the model output is coverted back using a natural logarithmic function
+
+        Args:
+        weight (float) - the model weights
+        feature_data_i (float) - the current feature_data
+        """
+        neuron_values = [np.exp(feature_data_i)]
+        for w in weight:
+            layers = w @ neuron_values[-1]
             neuron_values.append(layers)
 
         return np.log(neuron_values[-1])
--- Bayesian_Neural_Network.py
+++ Bayesian_Neural_Network.py
@@ -8,115 +8,176 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class bayesian_neural_network():
-    def __init__(self, input_layer, hidden_layers, output_layer, feature_data, target_data, learning_rate):
-        self.model_structure = np.concatenate((input_layer, hidden_layers, output_layer))
-        self.feature_data = np.exp(feature_data)
-        self.target_data = np.exp(target_data)
-
-        self.error = []
-        self.variance = []
-
+    def __init__(self, input_layer, hidden_layers, output_layer, feature_data, target_data, learning_rate, window_size):
+        self.model_structure = np.concatenate((input_layer, hidden_layers, output_layer)) # a list containing number of neurons on each layers
+        # create variables for both feature and target data
+        self.feature_data = feature_data
+        self.target_data = target_data
+        # standardize both feature and target data by applying an exponential function, then divide it with the max value of the feature data
+        self.feature_data_scaled = (np.exp(feature_data).reshape(-1, window_size) / np.max(np.exp(feature_data), axis=1)).reshape(-1, window_size, 1)
+        self.target_data_scaled = (np.exp(target_data).reshape(-1, 1) / np.max(np.exp(feature_data), axis=1)).reshape(-1, 1, 1)
+        # empty list to store all errors 
+        self.mean_error = []
+        self.variance_error = []
+        # create instances of class for the forward and backward propagation object
         self.bnn_fp = bnn_forward_propagation()
         self.bnn_pbp = bnn_probabilistic_back_propagation(learning_rate)
 
     def _generate_m(self, n_origin_neurons, n_destination_neurons):
+        """
+        create an initial weight's mean for all neurons in a layer
+
+        args:
+        n_origin_neurons (integer) - number of neurons in the previous layer
+        n_destination_neurons (integer) - number of neurons in the current layer
+        """
         return np.random.random(size=(n_destination_neurons, n_origin_neurons))
 
     def _generate_v(self, n_origin_neurons, n_destination_neurons):
+        """
+        create an initial weight's variance for all neurons in a layer, ensuring that the value is positive
+
+        args:
+        n_origin_neurons (integer) - number of neurons in the previous layer
+        n_destination_neurons (integer) - number of neurons in the current layer
+        """
         return np.abs(np.random.random(size=(n_destination_neurons, n_origin_neurons)))
         
     def generate_m(self):
+        """
+        automate the process of generating all initial weight's mean on all layers
+        """
         self.m = [self._generate_m(n_origin_neurons, n_destination_neurons) for n_origin_neurons, n_destination_neurons in zip(self.model_structure[:-1], self.model_structure[1:])]
 
         return
 
     def generate_v(self):
+        """
+        automate the process of generating all initial weight's variance on all layers
+        """
         self.v = [self._generate_v(n_origin_neurons, n_destination_neurons) for n_origin_neurons, n_destination_neurons in zip(self.model_structure[:-1], self.model_structure[1:])]
     
         return
 
-    def calculate_rmse_error(self, target_data_i, pred_mean_i):
-        return (np.log(target_data_i) - pred_mean_i)[0, 0] ** 2
-    
-    def calculate_prediction_variance(self, target_data_i, pred_variance_i):
-        return np.abs(np.log(target_data_i) - pred_variance_i)[0 ,0]
+    def _calculate_prediction_mean_error(self, target_data_i, pred_mean_i):
+        """
+        calculate the mean squared error for the mean of the prediction
 
-    def _standardize_feature_data(self, feature_data_i):
-        return feature_data_i / np.max(feature_data_i)
+        Args:
+        target_data_i (float) - the current feature data used to determine the mean of prediction
+        pred_mean_i (float) - the current mean of prediction
+        """
+        return (target_data_i - pred_mean_i)[0, 0] ** 2
     
-    def _standardize_target_data(self, feature_data_i, target_data_i):
-        return target_data_i / np.max(feature_data_i)
+    def _calculate_prediction_variance_error(self, target_data_i, pred_variance_i):
+        """
+        calculate the mean squared error for the mean of the prediction
+
+        Args:
+        target_data_i (float) - the current feature data used to determine the mean of prediction
+        pred_variance_i (float) - the current variance of prediction
+        """
+        return np.abs(target_data_i - pred_variance_i)[0 ,0]
 
     def train(self, epochs):
+        """
+        perform forward propagation to acquire all necessary variables, then perform backward propagation to optimize the model weight's mean and variance
+
+        Args:
+        epochs (integer) - the number of iteration of training using the whole feature dataset
+        """
         for epoch in range(epochs):
-            for feature_data_i, target_data_i in zip(self.feature_data, self.target_data):
-                forward_propagation_result = self.bnn_fp.forward_propagation(self._standardize_feature_data(feature_data_i), 
+            zipped_var = zip(self.feature_data, self.target_data, self.feature_data_scaled, self.target_data_scaled)
+            for feature_data_i, target_data_i, feature_data_scaled_i, target_data_scaled_i in zipped_var:
+                # perform forward propagation
+                forward_propagation_result = self.bnn_fp.forward_propagation(feature_data_scaled_i, 
                                                                                 self.m, 
                                                                                 self.v, 
                                                                                 self.model_structure)
+                # perform backward propagation to acquire the derivatives for optimizing the model's weights
                 d_logz_over_m, d_logz_over_v = self.bnn_pbp.calculate_derivatives(self.model_structure, 
-                                                                                    self._standardize_target_data(feature_data_i, target_data_i), 
+                                                                                    target_data_scaled_i, 
                                                                                     self.m, 
                                                                                     self.v, 
                                                                                     forward_propagation_result)
+                # optimize the model's weights
                 self.m = self.bnn_pbp.optimize_m(self.m, 
                                                     self.v, 
                                                     d_logz_over_m)
-                                                    
                 self.v = self.bnn_pbp.optimize_v(self.m, 
                                                     self.v, 
                                                     d_logz_over_m, 
                                                     d_logz_over_v)
-                
+                # calculate the mean and variance of the predicton
                 pred_mean_i = self.bnn_fp.feed_forward_neural_network(self.m, feature_data_i)
                 pred_var_i = self.bnn_fp.feed_forward_neural_network(self.v, feature_data_i)
-
-                self.error.append(self.calculate_rmse_error(target_data_i, 
-                                                                pred_mean_i))
-                self.variance.append(self.calculate_prediction_variance(target_data_i,
-                                                                            pred_var_i))
+                # calculate the errors and store in the list
+                self.mean_error.append(self._calculate_prediction_mean_error(target_data_i, 
+                                                                            pred_mean_i))
+                self.variance_error.append(self._calculate_prediction_variance_error(target_data_i,
+                                                                                    pred_var_i))
 
         return         
     
     def visualize_performance(self):
+        """
+        visualize the model performance throughout the training process
+        """
         fig, (ax1, ax2) = plt.subplots(2, 1)
         fig.set_size_inches(20, 10)
 
-        ax1.plot(self.error)
-        ax1.set_title('RMSE Throughout Training')
-        ax2.plot(self.variance)
+        ax1.plot(self.mean_error)
+        ax1.set_title('Mean MSE Throughout Training')
+        ax2.plot(self.variance_error)
         ax2.set_title('Variance Throughout Training')
 
         fig.show()
 
         return 
 
-    def _sampling_from_normal_dist(self, pred_mean_i, var_pred_i):
-        return [np.random.normal(pred_mean_i, var_pred_i ** 0.5) for _ in range(250)]
+    def _sampling_from_normal_dist(self, pred_mean_i, pred_var_i):
+        """
+        take sample from a normal distirbution with the mean and variance provided by the input of the function
+
+        Args:
+        pred_mean_i (float) - the mean of the prediction
+        pred_var_i (float) - the variance of the prediction
+        """
+        return [np.random.normal(pred_mean_i, pred_var_i ** 0.5) for _ in range(250)]
     
     def predict_on_seen_data(self):
+        """
+        create predictions on the data used in the training process
+        """
+        # create a list containing mean and variance of the prediction for each feature data
         pred_mean = [self.bnn_fp.feed_forward_neural_network(self.m, feature_data_i)[0, 0] for feature_data_i in self.feature_data]
-        pred_var = [np.abs(self.bnn_fp.feed_forward_neural_network(self.v, feature_data_i) - np.log(self.target_data[i]))[0, 0] for i, feature_data_i in enumerate(self.feature_data)]
-
+        pred_var = [self._calculate_prediction_variance_error(target_data_i, self.bnn_fp.feed_forward_neural_network(self.v, feature_data_i)) for feature_data_i, target_data_i in zip(self.feature_data, self.target_data)]
+        # pred_var = [np.abs(self.bnn_fp.feed_forward_neural_network(self.v, feature_data_i) - np.log(self.target_data[i]))[0, 0] for i, feature_data_i in enumerate(self.feature_data)]
+        # sampling from a normal distribution
         normal_sample = [self._sampling_from_normal_dist(pred_mean_i, pred_var_i) for pred_mean_i, pred_var_i in zip(pred_mean, pred_var)]
+        # calculate the mean and variance for each collections of samples
         self.prediction_mean = np.mean(normal_sample, axis=1)
         self.prediction_std = np.std(normal_sample, axis=1)
 
         return
     
-    def visualize_predictions(self):
+    def visualize_predictions_on_seen_data(self):
+        """
+        visualize the preiditions on data used throughout training alongside its confidence interval
+        """
         fig, ax = plt.subplots()
         fig.set_size_inches(20, 10)
 
+        # calculate the upper and lower bound of the prediction
         self.upper_bound = self.prediction_mean + self.prediction_std
         self.lower_bound = self.prediction_mean - self.prediction_std
 
-        x_plot = np.arange(0, len(self.feature_data))
-        ax.plot(x_plot, np.log(self.target_data.reshape(1, -1)[0]), color='black', label='Mean')
-        ax.plot(x_plot, self.prediction_mean, color='green', label='Mean')
-        ax.plot(x_plot, self.upper_bound, color='red', label='Upper')
-        ax.plot(x_plot, self.lower_bound, color='red', label='Lower')
-        ax.fill_between(x_plot, self.upper_bound, self.lower_bound, color="blue", alpha=0.15)
+        x_axis = np.arange(0, len(self.feature_data))
+        ax.plot(x_axis, self.target_data.reshape(1, -1)[0], color='black', label='Mean')
+        ax.plot(x_axis, self.prediction_mean, color='green', label='Mean')
+        ax.plot(x_axis, self.upper_bound, color='red', label='Upper')
+        ax.plot(x_axis, self.lower_bound, color='red', label='Lower')
+        ax.fill_between(x_axis, self.upper_bound, self.lower_bound, color="blue", alpha=0.15)
         ax.legend(['Data', 'Prediction Mean', 'Upper Bound x% Confidence Interval', 'Lower Bound x% Confidence Interval', 'Confidence Interval'])
 
         fig.show()
